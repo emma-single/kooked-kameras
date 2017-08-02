4ac90ca091436780bea57e86ebcee3e70a0225e3
emma-single <emmamollysingle@gmail.com>
2017-08-03 11:02:17 +1200

    added routes in app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,3 +1,13 @@
 import React from 'react'
+import { HashRouter as Router, Route } from 'react-router-dom'
+
+import Home from './Home'
+import Nav from './Nav'
+import Model from './Model'
+import ModelsList from './ModelsList'
+import Brands from './Brands'
+import Blog from './Blog'
+import About from './About'
+import Funfacts from './Funfacts'
 
 export default App
