58fd449899a6f130c19d424cab7faa7a9ad36f0a
emma-single <emmamollysingle@gmail.com>
2017-08-23 11:15:49 +1200

    took away capitalise

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -9,9 +9,9 @@ const Nav = ({ location }) => {
   return (
     <div className='nav'>
       <ul>
-        {rankList.map((navList, key) => {
-          const normal = <li key={key}><Link to={`/list/${navList}`}>{capitalize(navList)}</Link></li>
-          const selected = <li key={key}><strong><Link to={`/list/${navList}`}>{capitalize(navList)}</Link></strong></li>
+        {navList.map((navList, key) => {
+          const normal = <li key={key}><Link to={`/list/${navList}`}></Link></li>
+          const selected = <li key={key}><strong><Link to={`/list/${navList}`}></Link></strong></li>
           return location.pathname.indexOf(navList) > 0 ? selected : normal
         })}
       </ul>
